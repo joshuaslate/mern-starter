698651d6d46221e95ac68a698338c9384144c428
Joshua.Slate <Joshua.Slate@procentive.com>
2016-07-13 12:08:38 -0500

    Removing console log of user info

--- server/config/passport.js
+++ server/config/passport.js
@@ -38,11 +38,10 @@ const jwtOptions = {
 
 // Setting up JWT login strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
-  User.findById(payload._doc._id, function(err, user) {
+  User.findById(payload._id, function(err, user) {
     if (err) { return done(err, false); }
 
     if (user) {
-      console.log(user);
       done(null, user);
     } else {
       done(null, false);
