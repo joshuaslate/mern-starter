006281735ae529a1ceb5e42f2d7dad78489addd3
Joshua Slate <josh.a.slate@gmail.com>
2016-11-29 17:30:03 -0600

    Linting and de-politicizing :)

--- server/tests/loginAndAuthTest.js
+++ server/tests/loginAndAuthTest.js
@@ -1,25 +1,24 @@
-const should = require('should'),
-  request = require('supertest'),
-  app = require('../index'),
-  URI = require('./spec_helper').URI,
-  mongoose = require('mongoose');
+const should = require('should');
+const request = require('supertest');
+const app = require('../index');
+const URI = require('./spec_helper').URI;
+const mongoose = require('mongoose');
 
-const validUser = {password: 'secret', lastName: 'Trump', firstName: 'Donald', email: 'donald.trump@gov.us'};
+const validUser = { password: 'secret', lastName: 'Doe', firstName: 'John', email: 'johndoe@exampledomain.com' };
 
 
 describe('************* AUTH REGISTRATION *************', () => {
-
-  after(done=> {
+  after((done) => {
     const colls = 'users';
-    mongoose.connection.collections[colls].drop(err=> done());
+    mongoose.connection.collections[colls].drop(() => done());
   });
 
-  it('should FAIL [422] to create a user without paramters', (done) => {
+  it('should FAIL [422] to create a user without parameters', (done) => {
     request(app)
       .post('/api/auth/register')
       .set('X-Real-IP', URI)
       .expect(422)
-      .end((err, res)=> {
+      .end((err, res) => {
         if (err) done(err);
 
         res.body.error.should.be.eql('You must enter an email address.');
@@ -27,14 +26,14 @@ describe('************* AUTH REGISTRATION *************', () => {
       });
   });
 
-  it('should FAIL [422] to create a user with incomplete paramter', (done) => {
+  it('should FAIL [422] to create a user with incomplete parameters', (done) => {
     request(app)
       .post('/api/auth/register')
       .set('X-Real-IP', URI)
       .type('form')
-      .send({email: 'trump@gov.us', lastName: 'Trump', firstName: 'Donald'})
+      .send({ email: 'johndoe@exampledomain.com', lastName: 'Doe', firstName: 'John' })
       .expect(422)
-      .end((err, res)=> {
+      .end((err, res) => {
         if (err) done(err);
         res.body.error.should.be.eql('You must enter a password.');
         done();
@@ -48,7 +47,7 @@ describe('************* AUTH REGISTRATION *************', () => {
       .type('form')
       .send(validUser)
       .expect(201)
-      .end((err, res)=> {
+      .end((err, res) => {
         if (err) done(err);
         res.body.token.should.be.an.instanceOf(String);
         res.body.user.should.be.an.instanceOf(Object);
@@ -63,33 +62,31 @@ describe('************* AUTH REGISTRATION *************', () => {
       .type('form')
       .send(validUser)
       .expect(422)
-      .end((err, res)=> {
+      .end((err, res) => {
         if (err) done(err);
         res.body.error.should.be.eql('That email address is already in use.');
-        done()
+        done();
       });
-
   });
 });
 
 describe('************* AUTH LOGIN *************', () => {
-
-  before((done)=> {
+  before((done) => {
     request(app)
       .post('/api/auth/register')
       .set('X-Real-IP', URI)
       .type('form')
       .send(validUser)
       .expect(201)
-      .end((err, res)=> {
+      .end((err) => {
         if (err) done(err);
         done();
       });
   });
 
-  after((done)=> {
+  after((done) => {
     const colls = 'users';
-    mongoose.connection.collections[colls].drop(err=> done());
+    mongoose.connection.collections[colls].drop(() => done());
   });
 
   it('should FAIL [400] to login without parameters', (done) => {
@@ -104,16 +101,16 @@ describe('************* AUTH LOGIN *************', () => {
       .post('/api/auth/login')
       .set('X-Real-IP', URI)
       .type('form')
-      .send({wrongparam: 'err'})
+      .send({ wrongparam: 'err' })
       .expect(400, done);
   });
 
-  it('should FAIL [401] to login with invalid credential', (done) => {
+  it('should FAIL [401] to login with invalid credentials', (done) => {
     request(app)
       .post('/api/auth/login')
       .set('X-Real-IP', URI)
       .type('form')
-      .send({email: 'err', password: '22'})
+      .send({ email: 'err', password: '22' })
       .expect(401, done);
   });
 
@@ -124,7 +121,7 @@ describe('************* AUTH LOGIN *************', () => {
       .type('form')
       .send(validUser)
       .expect(200)
-      .end((err, res)=> {
+      .end((err, res) => {
         if (err) done(err);
         res.body.token.should.be.an.instanceOf(String);
         res.body.user.should.be.an.instanceOf(Object);
