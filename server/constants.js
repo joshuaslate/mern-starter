1ac524a914fe23159bb6d641232718bc21a84b14
Tarkan Anlar <tarkanlar@gmail.com>
2016-11-29 02:28:48 +0200

    It worked when I changed those

--- server/constants.js
+++ server/constants.js
@@ -1,7 +1,7 @@
-const ROLE_MEMBER = 'member';
-const ROLE_CLIENT = 'client';
-const ROLE_OWNER = 'owner';
-const ROLE_ADMIN = 'admin';
+const ROLE_MEMBER = 'Member';
+const ROLE_CLIENT = 'Client';
+const ROLE_OWNER = 'Owner';
+const ROLE_ADMIN = 'Admin';
 
 module.exports = {
   ROLE_MEMBER,
